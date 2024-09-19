b3b6c1edde6be50837ff0857b9e1cbfb1dee1045
xenosoftpd <76124002+xenosoftpd@users.noreply.github.com>
2024-09-19 17:25:26 +0900

    main

--- 02_variables.dart
+++ 02_variables.dart
@@ -1,64 +1,25 @@
-void main(List<String> args) {
-  //not set initial value using var
-  var a;
-  a = 'flutter meetup Ottawa';
-  print(a);
+void main(List<String> arguments) {
+  // Numbers: int
+  int score = 23;
+  var count = 23; // It is inferred as integer automatically by compiler
+  int hexValue = 0xEADEBAEE;
 
-  a = 123;
-  print(a);
+  // Numbers: double
+  double percentage = 93.4;
+  var percent = 82.533;
+  double exponents = 1.42e5;
 
-  a = true;
-  print(a);
+  // Strings
+  String name = "Henry";
+  var company = "Google";
 
-  a = {'key': 'value'};
-  print(a);
+  // Boolean
+  bool isValid = true;
+  var isAlive = false;
 
-  a = ['abc'];
-  print(a);
+  print(score);
+  print(exponents);
 
-  // if var variable is initialized
-  var a1 = "flutter meetup ottawa";
-  print(a1);
-  a = 123;
-  print(a1);
-
-  //using Object for dynamic any type
-  Object b = 'Flutter meetup Ottawa';
-  print(b);
-
-  b = 123;
-  print(b);
-
-  b = [12345];
-  print(b);
-
-  //b.p(); -> Object b doesn;t have method p(), the Dart compiler shows the error
-
-  dynamic c = "flutter meetup ottawa";
-  print(c);
-
-  c = 1234;
-  print(c);
-
-  //c.p(); // no error is detected by compiler, but will receive an error in  runtime.
-
-  String d;
-  d = 'flutter meetup ottawa';
-  print(d);
-  // d = 123; // cannot change the type
-
-  String e0 = 'flutter meetup ottawa';
-  print(e0);
-  int e1 = 123;
-  print(e1);
-  double e2 = 0.12;
-  print(e2);
-  num e3 = 1000.012;
-  print(e3);
-  bool e4 = false;
-  print(e4);
-  DateTime e5 = new DateTime.now();
-  print(e5);
-  List e6 = [e1, e2, e3, e4, e5];
-  print(e6);
+  // NOTE: All data types in Dart are Objects.
+  // Therefore, their initial value is by default 'null'
 }
