6810e5d6a753796b9dc52df4a43e564b62f2165c
xenosoftpd <76124002+xenosoftpd@users.noreply.github.com>
2024-09-20 01:02:11 +0900

    main

--- 02_variables.dart
+++ 02_variables.dart
@@ -1,31 +1,15 @@
 void main() {
   // Numbers: int
-  int score = 23;
-  var count = 23; // It is inferred as integer automatically by compiler
-  int hexValue = 0xEADEBAEE;
+  var length = 20;
+  int width = 20; // It is inferred as integer automatically by compiler
+  var area = length * width;
+
+  print('Area = $area');
 
   // Numbers: double
-  double percentage = 93.4;
-  var percent = 82.533;
-  double exponents = 1.42e5;
 
   double distanceMile = 1;
   double distanceKm = distanceMile * 1.609;
 
   print('$distanceMile mile =  $distanceKm km');
-
-  // Strings
-  String name = "John";
-  var company = "Google";
-
-  // Boolean
-  bool isValid = true;
-  var isAlive = false;
-
-  print(score);
-  print(exponents);
-  print(company);
-
-  // NOTE: All data types in Dart are Objects.
-  // Therefore, their initial value is by default 'null'
 }
